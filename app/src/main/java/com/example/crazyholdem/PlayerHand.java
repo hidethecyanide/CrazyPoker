748f6237ef58cc087f576677dbedd82ee29a1809
hidethecyanide <hidethecyanide314@gmail.com>
2024-12-29 14:54:30 -0600

    removed extraneous boolean variables

--- app/src/main/java/com/example/crazyholdem/PlayerHand.java
+++ app/src/main/java/com/example/crazyholdem/PlayerHand.java
@@ -3,74 +3,26 @@ package com.example.crazyholdem;
 import java.util.ArrayList;
 import java.util.List;
 public class PlayerHand {
-    boolean highCard;
-    boolean pair;
-    boolean twoPair;
-    boolean threeKind;
-    boolean straight;
-    boolean flush;
-    boolean fullHouse;
-    boolean fourKind;
-    boolean fiveKind;
-    boolean straightFlush;
-    boolean royalFlush;
-    int numHighHand;
-    int numLowHand;
+
     int handStrength;
     ArrayList<Card> hand;
     ArrayList<Card> scoringHand;
 
     // Default constructor
     public PlayerHand() {
-        this.highCard = true;
-        this.pair = false;
-        this.twoPair = false;
-        this.threeKind = false;
-        this.straight = false;
-        this.flush = false;
-        this.fullHouse = false;
-        this.fourKind = false;
-        this.fiveKind = false;
-        this.straightFlush = false;
-        this.royalFlush = false;
+
         this.hand = new ArrayList<>();
         handStrength = 0;
     }
-
-    // Method to reset all hand types (useful for new rounds)
-    public void resetHandTypes() {
-        this.pair = false;
-        this.twoPair = false;
-        this.threeKind = false;
-        this.straight = false;
-        this.flush = false;
-        this.fullHouse = false;
-        this.fourKind = false;
-        this.fiveKind = false;
-        this.straightFlush = false;
-        this.royalFlush = false;
-    }
-    public void setNumHighHand(int numHighHand){
-        this.numHighHand = numHighHand;
-    }
-    public void setNumLowHand(int numLowHand){
-        this.numLowHand = numLowHand;
-    }
-    public int getNumHighHand(){
-        return numHighHand;
-    }
-    public int getNumLowHand() {
-        return numLowHand;
-    }
-
-    public void isHighCard(List<Card> hand) {
-        highCard = true;
-
+    public boolean isHighCard(List<Card> hand) {
+        return true;
     }
     // Example hand evaluator (you would expand this)
-    public void isPair(List<Card> hand){}
-
-    public void isTwoPair(List<Card> hand) {
+    public boolean isPair(List<Card> hand) {
+        return true;
+    }
+    public boolean isTwoPair(List<Card> hand) {
+        return true;
     }
     public void clearHand(){
         hand.clear();
@@ -84,4 +36,17 @@ public class PlayerHand {
     public int getHandStrength(){
         return handStrength;
     }
+
+    public void setHandStrength(int handStrength){
+        this.handStrength = handStrength;
+    }
+    public void resetHand(){
+        handStrength = 0;
+        hand.clear();
+        scoringHand.clear();
+    }
+    public int evaluateHand(){
+        return handStrength;
+    }
+
 }
